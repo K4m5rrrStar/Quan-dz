284bb3479e5b9e06d4a8e6e1dcd11121c288cc2c
Your Name <you@example.com>
2022-06-04 01:15:30 +0700

    first commit

---
+++ BanSachOnline/contact.php
@@ -0,0 +1,47 @@
+<?php 
+	include "head.php"
+	?>
+<?php
+$title ="Shop huy";
+$name ="Điện thoai";
+?>
+<?php 
+	include "top.php"
+    ?>
+    <?php 
+	include "Header.php"
+	?>
+	<?php 
+	include "navigation.php"
+	?>
+	<!--//////////////////////////////////////////////////-->
+	<!--///////////////////Contact Page///////////////////-->
+	<!--//////////////////////////////////////////////////-->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1>Liên hệ với chúng tôi</h1></div>
+				</div>
+				<div class="col-md-6">
+					<p><span class="glyphicon glyphicon-home"></span>Yên nghĩa, Hà Đông, Hà Nội</p>
+					<p><span class="glyphicon glyphicon-earphone"></span> 0396181976</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> 20010879@st.phenikaa-uni.edu.vn</p>
+					<iframe width="400" height="400" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.7826576818084!2d105.7452841147621!3d20.961241586035097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313452efff394ce3%3A0x391a39d4325be464!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBQaGVuaWthYQ!5e0!3m2!1svi!2s!4v1653962287571!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
